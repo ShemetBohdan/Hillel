b41f32c2c97c4f10e84ddf2a5f925d6c4738c565
Shemet Bohdan <shemet.bohdan01@gmail.com>
2022-04-27 09:09:24 +0300

    add homework

--- src/lesson10/App.java
+++ src/lesson10/App.java
@@ -1,58 +1,29 @@
 package lesson10;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class App {
+public class App  {
 
     public static void main(String[] args) {
 
-        List<Ship> ships = new ArrayList<>();
+        ArrayL<Integer> arrayL = new ArrayL<>();
 
-        Ship barge = new Ship("barge");
-        Ship dredger = new Ship("dredger");
-        Ship dugout = new Ship("dugout");
-        Ship galley = new Ship("galley");
-        Ship gondola = new Ship("gondola");
-        Ship hovercraft = new Ship("hovercraft");
-        Ship icebreaker = new Ship("icebreaker");
+        arrayL.add(10);
+        arrayL.add(20);
+        arrayL.add(30);
+        arrayL.add(10);
+        arrayL.add(20);
 
-        ships.add(barge);
-        ships.add(dredger);
-        ships.add(dugout);
-        ships.add(galley);
-        ships.add(gondola);
-        ships.add(hovercraft);
-        ships.add(icebreaker);
-
-        System.out.println("show collection: "+ ships);
-
-        System.out.println("\n"+ "size: " + ships.size());
-        System.out.println("collection is empty: " + ships.isEmpty());
-        System.out.println("collection contains element: " + ships.contains(gondola));
-        System.out.println( "which index element <galley>: " + ships.indexOf(galley));
-        ships.remove(dugout);
-        System.out.println("\n"+"delete element <dugout> and show collection:" + ships);
-
-        Ship tanker = new Ship("tanker");
-
-        System.out.println(" We added an element instead of : " + ships.set(2,tanker));
-        System.out.println("\n"+ships);
-        System.out.println("Show elements from 1 to 3: "+ ships.subList(1,3));
-
-        List <Ship> submarines = new ArrayList<>();
-
-        Ship holland  = new Ship("holland");
-        Ship nautilus  = new Ship("nautilus");
-        Ship rainbow  = new Ship("rainbow");
-
-        submarines.add(holland);
-        submarines.add(nautilus);
-        submarines.add(rainbow);
-        submarines.addAll(0,ships);
-        System.out.println("\n"+ submarines);
-        System.out.println("\n"+"show the name of the element at index five: " + submarines.get(5));
-        submarines.clear();
-        System.out.println(submarines);
+        System.out.println("size: " + arrayL.size());
+        System.out.println("collection is empty: " + arrayL.isEmpty());
+        System.out.println("what element is under for index <2>: "+ arrayL.get(2));
+        System.out.println(arrayL);
+        arrayL.remove(2);
+        System.out.println(arrayL);
+        arrayL.set(0,11);
+        System.out.println(arrayL);
+        System.out.println(arrayL.iterator().hasNext());
+        arrayL.add(0,15);
+        System.out.println(arrayL);
+        System.out.println("which index element <20>: " + arrayL.indexOf(20));
+        System.out.println("which index element <20>: " + arrayL.lastIndexOf(20));
     }
 }
